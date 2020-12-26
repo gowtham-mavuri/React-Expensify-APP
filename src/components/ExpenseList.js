da45a0975ce7d2c676eea49f29df0b6ff459ce33
Gowtham Mavuri <gowtham2mavuri@gmail.com>
2020-12-26 21:38:22 +0530

    added expense summary

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,8 +4,14 @@ import ExpenseListItem from './ExpenseListItem';
 import expenses from '../selectors/expenses';
 
 const ExpenseList = (props) => {
+    const expenses = props.expenses.map((expense)=>expense.amount);
+                                
+    const totalExpensesSum = expenses.reduce((sum,val)=>sum+val,0);
+    const totalExpensesCount = expenses.length;
     return (
         <div>
+            <h2>No of Expenses : { totalExpensesCount }</h2>
+            <h2>Total Spent : { totalExpensesSum }</h2>
             <h3>Expenses List</h3>
             {props.expenses.map((expense)=>{
                 return <ExpenseListItem key={expense.id}{ ...expense } />
