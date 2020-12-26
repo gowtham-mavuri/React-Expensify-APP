57002d3033ec302e25bcaa6ca6385e89b24904bf
Gowtham Mavuri <gowtham2mavuri@gmail.com>
2020-12-26 16:03:40 +0530

    Removed static data in expenses

--- src/app.js
+++ src/app.js
@@ -10,18 +10,19 @@ import './styles/styles.scss';
 import AppRouter from './routers/AppRouter';
 
 const store = configureStore();
-/*  store.subscribe(()=>{
+/*  
+    store.subscribe(()=>{
         const state = store.getState();
         const expenses=getFilteredData(state.expenses,state.filters);
         console.log(expenses);
     });
+
+
+
+    store.dispatch(addExpense({ description:'Home Rent',amount:5000,createdAt:100 }));
+    store.dispatch(addExpense({ description:'Food Bill',amount:3000,createdAt:1000 }));
+
 */
-
-
-store.dispatch(addExpense({ description:'Home Rent',amount:5000,createdAt:100 }));
-store.dispatch(addExpense({ description:'Food Bill',amount:3000,createdAt:1000 }));
-
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
